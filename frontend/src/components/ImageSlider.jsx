0349fe6b2be3143c627998dd48c2d92f69be3cb8
mds_sayyad <155095438+saanisayyad@users.noreply.github.com>
2025-12-03 17:33:31 +0530

    popup window changede

--- frontend/src/components/ImageSlider.jsx
+++ frontend/src/components/ImageSlider.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import { RxDoubleArrowRight, RxDoubleArrowLeft } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 import Slab1 from "../assets/ProductImages/Slab1.jpeg";
 import Slab2 from "../assets/ProductImages/Slab2.jpeg";
 import Slab3 from "../assets/ProductImages/Slab3.jpeg";
@@ -11,29 +12,35 @@ import Slab4 from "../assets/ProductImages/Slab4.jpeg";
 import Slab5 from "../assets/ProductImages/Slab5.jpeg";
 
 const ImageSlider = () => {
-  const images = [
-    Slab1,
-    Slab2,
-    Slab3,
-    Slab4,
-    Slab5
-  ];
+  const images = [Slab1, Slab2, Slab3, Slab4, Slab5];
 
   const [popupOpen, setPopupOpen] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
+  // Close popup with ESC key
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === "Escape") setPopupOpen(false);
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
 
   return (
     <>
       {/* ---------- MAIN SLIDER ---------- */}
       <div className="relative">
         {/* CUSTOM ARROWS MUST COME BEFORE SWIPER */}
-        <button className="prev-main absolute left-2 top-1/2 -translate-y-1/2 z-20 
-                   bg-(--brand-bg) rounded-full p-1 ">
-          <RxDoubleArrowLeft  size={30} />
+        <button
+          className="prev-main absolute left-2 top-1/2 -translate-y-1/2 z-20 
+                   bg-(--brand-bg) rounded-full p-1 "
+        >
+          <RxDoubleArrowLeft size={30} />
         </button>
 
-        <button className="next-main absolute right-2 top-1/2 -translate-y-1/2 z-20 
-                   bg-(--brand-bg) rounded-full p-1 ">
+        <button
+          className="next-main absolute right-2 top-1/2 -translate-y-1/2 z-20 
+                   bg-(--brand-bg) rounded-full p-1 "
+        >
           <RxDoubleArrowRight size={30} />
         </button>
 
@@ -57,7 +64,7 @@ const ImageSlider = () => {
                   setActiveIndex(i);
                   setPopupOpen(true);
                 }}
-                className="w-full h-64 sm:h-80 md:h-[360px] lg:h-[416px]
+                className="w-full h-auto
                            object-cover rounded-lg shadow-xl cursor-pointer"
               />
             </SwiperSlide>
@@ -66,52 +73,73 @@ const ImageSlider = () => {
       </div>
 
       {/* ---------- POPUP SLIDER ---------- */}
-      {popupOpen && (
-        <div
-          className="fixed inset-0 bg-black/80 flex justify-center items-center z-50"
-          onClick={() => setPopupOpen(false)}
-        >
-          <div
-            className="relative w-[90%] max-w-4xl"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {/* POPUP ARROWS MUST BE BEFORE SWIPER */}
-            <button className="prev-popup absolute left-2 top-1/2 -translate-y-1/2 z-20 
-                   bg-(--brand-bg) rounded-full p-1">
-              <RxDoubleArrowLeft size={30} />
-            </button>
+{popupOpen && (
+  <div
+    className="fixed inset-0 bg-black/80 flex justify-center items-center z-50"
+    onClick={() => setPopupOpen(false)}
+  >
+    <div
+      className="relative w-[95%] max-w-5xl max-h-[90vh]"
+      onClick={(e) => e.stopPropagation()}
+    >
+      {/* CLOSE BUTTON (INSIDE IMAGE TOP-RIGHT) */}
+      <button
+        onClick={() => setPopupOpen(false)}
+        className="absolute top-1 right-41 z-30 
+                  font-bold 
+                   bg-(--brand-bg)
+                   rounded-full w-6 h-6 flex items-center justify-center"
+      >
+        <IoClose  />
+      </button>
 
-            <button className="next-popup absolute right-2 top-1/2 -translate-y-1/2 z-20 
-                   bg-(--brand-bg) rounded-full p-1">
-              <RxDoubleArrowRight size={30} />
-            </button>
+      {/* LEFT ARROW (INSIDE IMAGE) */}
+      <button
+        className="prev-popup absolute left-41 top-1/2 -translate-y-1/2 z-30 
+                   bg-(--brand-bg)
+                   rounded-full p-2"
+      >
+        <RxDoubleArrowLeft size={15} />
+      </button>
 
-            <Swiper
-              loop={true}
-              initialSlide={activeIndex}
-              modules={[Navigation]}
-              onBeforeInit={(swiper) => {
-                swiper.params.navigation.prevEl = ".prev-popup";
-                swiper.params.navigation.nextEl = ".next-popup";
-              }}
-              navigation={{
-                prevEl: ".prev-popup",
-                nextEl: ".next-popup",
-              }}
-            >
-              {images.map((img, i) => (
-                <SwiperSlide key={i}>
-                  <img
-                    src={img}
-                    className="w-full h-64 sm:h-80 md:h-[360px] lg:h-[416px]
-                               object-cover rounded-lg shadow-xl"
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-        </div>
-      )}
+      {/* RIGHT ARROW (INSIDE IMAGE) */}
+      <button
+        className="next-popup absolute right-41 top-1/2 -translate-y-1/2 z-30 
+                   bg-(--brand-bg)
+                  rounded-full p-2"
+      >
+        <RxDoubleArrowRight size={15} />
+      </button>
+
+      <Swiper
+        loop={true}
+        initialSlide={activeIndex}
+        modules={[Navigation]}
+        onBeforeInit={(swiper) => {
+          swiper.params.navigation.prevEl = ".prev-popup";
+          swiper.params.navigation.nextEl = ".next-popup";
+        }}
+        navigation={{
+          prevEl: ".prev-popup",
+          nextEl: ".next-popup",
+        }}
+      >
+        {images.map((img, i) => (
+          <SwiperSlide key={i}>
+            <img
+              src={img}
+              className="
+                max-h-[85vh] max-w-full mx-auto 
+                object-contain rounded-lg shadow-xl
+              "
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  </div>
+)}
+
     </>
   );
 };
